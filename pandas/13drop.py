53ca9b8ce146d32a796d9e46b2c37e93acc83b1a
PatilPunit <punitpatil770@gmail.com>
2026-04-19 15:18:36 +0530

    drop

---
+++ pandas/13drop.py
@@ -0,0 +1,12 @@
+import pandas as  pd
+import numpy as np
+
+df=pd.read_csv('/home/punit/Documents/Ds/pandas/players_20.csv')
+df=df[['short_name','long_name','age','dob','height_cm','weight_kg','club']]
+df.set_index('short_name',inplace=True)
+df.drop(index=['L. Messi'],axis=0,inplace=True)
+df.drop('Neymar Jr',inplace=True)
+print(df)
+df.drop(columns=['long_name'],axis=0,inplace=True)
+df.drop('age',axis=1,inplace=True)
+print(df)
--- pandas/masti.py
+++ pandas/masti.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as mt
 import pandas as pd
 import random as rd
-
+import pymongo as pm
 df = pd.read_csv("/home/punit/Documents/Ds/pandas/players_20.csv")
 
 df=df[['short_name','overall','potential','preferred_foot','skill_moves','international_reputation']]
@@ -14,3 +14,4 @@ print(df.loc['Wang Haijian','potential'])
 
 print(df.loc[:,'potential'])
 print(df.loc[df['potential']>90,'overall'])
+
