9837e5c6eed17e58ee0ab6758efc10af525e274c
PatilPunit <punitpatil770@gmail.com>
2026-05-01 00:01:02 +0530

    line

--- data_visulization/interactive.py
+++ data_visulization/interactive.py
@@ -3,6 +3,7 @@ from IPython.display import display , HTML
 import cufflinks as cf
 import matplotlib.pyplot as plt
 import plotly.express as ex
+import numpy as np
 cf.set_config_file(sharing='public',theme='ggplot',offline=True)
 
 df=pd.read_csv('/home/punit/Documents/Ds/data_visulization/population_total.csv')
@@ -15,6 +16,8 @@ print(dfpop)
 dfpop5=dfpop[['United States','India','Brazil','Indonesia','China']]
 print(dfpop5)
 
+# dfpop5.iplot(kind='line',colors='red')
+# plt.show()
 
 fig = ex.line(dfpop5)
 fig.show()
