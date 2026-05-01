5ff7b9b17f8bbe67864aa0973014ee542d230d74
PatilPunit <punitpatil770@gmail.com>
2026-05-02 00:11:26 +0530

    agg

--- pandas/20groupby.py
+++ pandas/20groupby.py
@@ -3,3 +3,13 @@ import pandas as pd
 path='/home/punit/Documents/Ds/pandas/Car_sales.csv'
 df = pd.read_csv(path)
 print(df)
+
+df = df[['Manufacturer','Model','Sales_in_thousands','Vehicle_type','Price_in_thousands','Engine_size','Horsepower']]
+dfsum=df.agg('sum')
+print(dfsum)
+print('_'*130)
+dfmean=df.drop(['Manufacturer','Model','Vehicle_type'],axis=1)
+dfmean=dfmean.agg('mean')
+print(dfmean)
+
+
