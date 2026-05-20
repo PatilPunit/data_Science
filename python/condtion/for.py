ee7c56af7b037e7c5b967320bea8ee070efc3618
PatilPunit <punitpatil770@gmail.com>
2026-05-20 15:49:33 +0530

    while2

--- python/condtion/for.py
+++ python/condtion/for.py
@@ -7,13 +7,19 @@ while x <= 3:
     print(x)
     x += 1
 
+
+
 x = 0
 
-while x < 6:
-
+while x < 5:
     x += 1
 
-    if x == 3:
+    if x == 2:
         continue
 
+    print(x)
+
+x = 1
+
+while x < 10:
     print(x)
