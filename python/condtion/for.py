26a7e0ea19cabaf9c783624d922656a59fc7e2da
PatilPunit <punitpatil770@gmail.com>
2026-05-21 11:23:40 +0530

    other

--- python/condtion/for.py
+++ python/condtion/for.py
@@ -40,18 +40,63 @@ x = [1,2,3]
 x.append([4,5])
 
 print(len(x))
+x = [[]] * 3
 
+x[0].append(1)
 
-a = [1,2]
-b = a
+print(x)
 
-print(a is b)
+a = (1, 2, [3,4])
 
-d = {"a":1, "b":2}
+a[2].append(5)
 
-print(d["a"])
+print(a)
 
-def test():
-    return
+x = {1,2,3}
+
+print(x.pop())
+
+def test(x=[]):
+    x.append(1)
+    return x
 
 print(test())
+print(test())
+
+print(0.1 + 0.2 == 0.3)
+
+a = [1,2,3]
+
+print(a[::-1])
+
+print([i*i for i in range(3)])
+
+a = "abc"
+
+print(a * 3)
+
+x = [1,2,3]
+
+print(x == x.copy())
+print(x is x.copy())
+
+x = [1,3,4,5,6,7,9,0,10]
+
+for i in x:
+    x.remove(i)
+
+print(x)
+
+def func(a, b=[]):
+    b.append(a)
+    return b
+import sys as s
+print(func(1))
+print(func(2))
+print(func(3, []))
+s.exit()
+import  os
+import random as rd
+
+l = [1,2,3,4,5]
+print(type(rd.shuffle(l)))
