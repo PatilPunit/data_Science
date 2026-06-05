8b3058b48bc04bffe885df4fb0b7ce7d2dc4fad6
PatilPunit <punitpatil770@gmail.com>
2026-06-05 14:15:07 +0530

    replave

--- data_clean/replave.py
+++ data_clean/replave.py
@@ -1,3 +1,17 @@
 import pandas as pd
 df=pd.read_csv('/home/punit/Documents/Ds/pandas/netflix.csv')
 print(df.head())
+
+#fillna() with mode , mean
+
+mode = df['rating'].mode()[0]#categorical  data
+
+df.fillna({'rating':mode},inplace=True)
+# print(df['rating'].fillna(mode,inplace=True))
+print(df['rating'].isnull().sum())
+
+# fillna with arbitary number
+
+df.fillna({'duration':'0'},inplace=True)
+print(df['duration'].isnull().sum())
+
